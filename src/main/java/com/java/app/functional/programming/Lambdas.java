a094621496436718de71c95da04ba4959b859453
lilhovhann <lilith.hovhann@gmail.com>
2021-11-10 10:24:38 +0400

    added more examples on Predicate

--- src/main/java/com/java/app/functional/programming/Lambdas.java
+++ src/main/java/com/java/app/functional/programming/Lambdas.java
@@ -1,6 +1,8 @@
 package com.java.app.functional.programming;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.*;
 
 /**
  *
@@ -8,16 +10,24 @@ import java.util.List;
  */
 public class Lambdas {
 
-   
     public static void main(String[] args) {
         List<Integer> nums = List.of(5, 4, 8, 9, 12, 0);
-        
+
         BiggerThanFive bf = new BiggerThanFive();
-        for(Integer num: nums){
-            
+        for (Integer num : nums) {
+
             //if number is bigger than five, test returns true and println method prints it
-            System.out.println(bf.test(num)? num: "");
+            System.out.println(bf.test(num) ? num : "");
         }
+
+        List<String> armenianWords = Arrays.asList("shun", "door", "dog");
+
+        Predicate<String> isArmenianWord = word -> word.equalsIgnoreCase("shun");
+
+        for (String w : armenianWords) {
+            System.out.println(isArmenianWord.test(w));
+        }
+
     }
-    
+
 }
